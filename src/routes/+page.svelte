8cfb4c5a90d69102afa46bf2f5d3de534a7e2174
George <gfrankpetersen@gmail.com>
2023-01-17 19:12:35 -0700

    main - adds sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<style lang="scss">
+    h1{
+        font-family: sans-serif;
+    }
+</style>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
